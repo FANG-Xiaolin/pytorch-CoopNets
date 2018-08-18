51341634645a35756c28a4329617f3c54abfc253
fxlfang <fxlfang@gmail.com>
2018-08-18 16:16:49 -0700

    fix typo

--- opts.py
+++ opts.py
@@ -8,34 +8,36 @@ class opts():
     def init(self):
         self.parser.add_argument('-num_epoch', type=int, default=300, help='training epochs')
         self.parser.add_argument('-batch_size', type=int, default=100, help='training batch size')
+        self.parser.add_argument('-test_size', type=int, default=1, help='How many images to generate during testing')
+        self.parser.add_argument('-test', help='add `-test` for testing')
         self.parser.add_argument('-nRow', type=int, default=12, help='how many rows of images in the output')
         self.parser.add_argument('-nCol', type=int, default=12, help='how many columns of images in the output')
         self.parser.add_argument('-img_size', type=int, default=64, help='output image size')
 
         self.parser.add_argument('-z_size', type=int, default=100, help='dimension of latent variable sample from latent space')
-        self.parser.add_argument('-category', default='cifar', help='training category')
+        self.parser.add_argument('-category', default='alp', help='training category')
         self.parser.add_argument('-data_path', default='./data/scene', help='path to data')
         self.parser.add_argument('-output_dir', default='./result_images', help='directory to save output synthesized images')
         self.parser.add_argument('-log_dir', default='./checkpoint', help='directory to save logs')
         self.parser.add_argument('-ckpt_dir', default='./checkpoint', help='directory to save checkpoints')
         self.parser.add_argument('-log_epoch', type=int, default=50, help='save checkpoint each `log_epoch` epochs')
 
-        self.parser.add_argument('-set', default='cifar', help='which dataset, scene/cifar')
+        self.parser.add_argument('-set', default='scene', help='which dataset, scene/cifar')
         self.parser.add_argument('-with_noise', type=bool, default=True, help='add noise during the langevin or not')
 
         #Generator Parameters
         self.parser.add_argument('-ckpt_des', default=None, help='load checkpoint for descriptor')
-        self.parser.add_argument('-sigma_gen', type=float, default=0.3,help='')
-        self.parser.add_argument('-langevin_step_num_gen', type=int, default=0, help='# hourglasses to stack')
-        self.parser.add_argument('-langevin_step_size_gen', type=float, default=0.1, help='# hourglasses to stack')
+        self.parser.add_argument('-sigma_gen', type=float, default=0.3,help='sigma of reference distribution')
+        self.parser.add_argument('-langevin_step_num_gen', type=int, default=0, help='langevin step number for generator')
+        self.parser.add_argument('-langevin_step_size_gen', type=float, default=0.1, help='langevin step size for generator')
         self.parser.add_argument('-lr_gen', type=float, default=0.0001,help='learning rate of generator')
         self.parser.add_argument('-beta1_gen', type=float, default=0.5,help='beta of Adam for generator')
 
         #Descriptor Parameters
         self.parser.add_argument('-ckpt_gen', default=None, help='load checkpoint for generator')
         self.parser.add_argument('-sigma_des', type=float, default=0.016, help='sigma of reference distribution')
-        self.parser.add_argument('-langevin_step_num_des', type=int, default=10, help='drop LR')
-        self.parser.add_argument('-langevin_step_size_des', type=float, default=0.002, help='# hourglasses to stack')
+        self.parser.add_argument('-langevin_step_num_des', type=int, default=10, help='langevin step number for descriptor')
+        self.parser.add_argument('-langevin_step_size_des', type=float, default=0.002, help='langevin step size for descriptor')
         self.parser.add_argument('-lr_des', type=float, default=0.01,help='learning rate of descriptor')
         self.parser.add_argument('-beta1_des', type=float, default=0.5,help='beta of Adam for descriptor')
 
